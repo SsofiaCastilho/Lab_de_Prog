e2aef1cd0baf805d0b3ecc8711fde78ac4507f86
Sofia Castilho Latini Gonçalves <sofia.goncalves@ufv.br>
2025-03-19 09:18:30 -0300

    página de login e conexão bd
    falta exibir mensagem de erro ou sucesso com as informações fornecidas pelo banco

---
+++ login_bd.php
@@ -0,0 +1,28 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "login");
+
+$email = 'sofiacastilho@gmail.com';
+$senha = '123456';
+
+$email = 'tatiane@gmail.com';
+$senha = '12345';
+
+$sql = "INSERT INTO usuario (email, senha) VALUES ('$email', '$senha')";
+
+$resultado = mysqli_query($con, $sql);
+
+
+if ($row = mysqli_fetch_assoc($resultado)) {
+    // Verifica a senha usando password_verify
+    if (password_verify($senha, $row['senha'])) {
+        $_SESSION['usuario'] = $email;
+        header("Location: index.html?msg=" . urlencode("Login feito com sucesso!"));
+    } else {
+        header("Location: index.html?msg=" . urlencode("Senha incorreta!"));
+    }
+} else {
+    header("Location: index.html?msg=" . urlencode("Usuário não encontrado!"));
+}
+
+mysqli_close($con);
