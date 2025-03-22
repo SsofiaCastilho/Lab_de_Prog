8d7f64eda8e5234485de02f2fe93b12a0e4ecd35
Sofia Castilho Latini Gonçalves <sofia.goncalves@ufv.br>
2025-03-22 10:05:01 -0300

    comentário
    comentei o teste

--- insercao_bd.php
+++ insercao_bd.php
@@ -11,10 +11,10 @@ $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
 $sql = "INSERT INTO usuario (email, senha) VALUES ('$email', '$senha_hash')";
 
-if (mysqli_query($con, $sql)) {
+/*if (mysqli_query($con, $sql)) {
     echo "Usuário inserido com sucesso!";
 } else {
     echo "Erro: " . mysqli_error($con);
-}
+}*/
 
 mysqli_close($con);
